f72f84454dc4e820b8b0d49635ff9edf4387491b
Nathalia de Luccas Filopoulos <nathiluccas@gmail.com>
2017-11-08 10:59:38 +0000

    wiki crud

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+require "faker"
+
+10.times do
+  articles = Article.create(
+    title: Faker::Beer.name,
+    content: Faker::Internet.domain_name
+  )
+
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
